ae633f5d917885d04511735144674fc69a777031
Viorel Tsigos <viorel.tsigos@gmail.com>
2025-11-02 14:44:23 +0100

    sheet 1: refinements for task 1
    changed answer for tasks a and c, modified CriticalPathFinder to be memory safer and adapted tests correspondingly

--- Blatt1/code/Aufgabe 1/src/tests.cpp
+++ Blatt1/code/Aufgabe 1/src/tests.cpp
@@ -1,237 +1,238 @@
 #include "gtest/gtest.h"
 #include "CriticalPathFinder.h"
 #include "Package.h"
+#include <memory>
 #include <vector>
 
 TEST(CriticalPath, PackageFromAssignment) {
-    Package a01{ 1, 3, {} };
-    Package a02{ 2, 4, {} };
-    Package a03{ 3, 5, {} };
-    Package a04{ 4, 4, { a01.get_id() } };
-    Package a05{ 5, 9, { a01.get_id() } };
-    Package a06{ 6, 4, { a02.get_id() } };
-    Package a07{ 7, 2, { a03.get_id() } };
-    Package a08{ 8, 4, { a04.get_id() } };
-    Package a09{ 9, 2, { a06.get_id() } };
-    Package a10{ 10, 3, { a06.get_id() } };
-    Package a11{ 11, 3, { a07.get_id() } };
-    Package a12{ 12, 2, { a05.get_id(), a08.get_id() } };
-    Package a13{ 13, 4, { a10.get_id(), a11.get_id() } };
-    Package a14{ 14, 7, { a09.get_id(), a12.get_id() } };
-    Package a15{ 15, 2, { a09.get_id(), a12.get_id() } };
-    Package a16{ 16, 4, { a13.get_id(), a15.get_id() } };
+    auto a01 = std::make_shared<Package>(1, 3, std::vector<int>{});
+    auto a02 = std::make_shared<Package>(2, 4, std::vector<int>{});
+    auto a03 = std::make_shared<Package>(3, 5, std::vector<int>{});
+    auto a04 = std::make_shared<Package>(4, 4, std::vector<int>{ a01.get()->get_id() });
+    auto a05 = std::make_shared<Package>(5, 9, std::vector<int>{ a01.get()->get_id() });
+    auto a06 = std::make_shared<Package>(6, 4, std::vector<int>{ a02.get()->get_id() });
+    auto a07 = std::make_shared<Package>(7, 2, std::vector<int>{ a03.get()->get_id() });
+    auto a08 = std::make_shared<Package>(8, 4, std::vector<int>{ a04.get()->get_id() });
+    auto a09 = std::make_shared<Package>(9, 2, std::vector<int>{ a06.get()->get_id() });
+    auto a10 = std::make_shared<Package>(10, 3, std::vector<int>{ a06.get()->get_id() });
+    auto a11 = std::make_shared<Package>(11, 3, std::vector<int>{ a07.get()->get_id() });
+    auto a12 = std::make_shared<Package>(12, 2, std::vector<int>{ a05.get()->get_id(), a08.get()->get_id() });
+    auto a13 = std::make_shared<Package>(13, 4, std::vector<int>{ a10.get()->get_id(), a11.get()->get_id() });
+    auto a14 = std::make_shared<Package>(14, 7, std::vector<int>{ a09.get()->get_id(), a12.get()->get_id() });
+    auto a15 = std::make_shared<Package>(15, 2, std::vector<int>{ a09.get()->get_id(), a12.get()->get_id() });
+    auto a16 = std::make_shared<Package>(16, 4, std::vector<int>{ a13.get()->get_id(), a15.get()->get_id() });
 
-    std::unordered_map<int, Package*> package_map = {
-        { a01.get_id(), &a01 }, { a02.get_id(), &a02 }, { a03.get_id(), &a03 }, { a04.get_id(), &a04 },
-        { a05.get_id(), &a05 }, { a06.get_id(), &a06 }, { a07.get_id(), &a07 }, { a08.get_id(), &a08 },
-        { a09.get_id(), &a09 }, { a10.get_id(), &a10 }, { a11.get_id(), &a11 }, { a12.get_id(), &a12 },
-        { a13.get_id(), &a13 }, { a14.get_id(), &a14 }, { a15.get_id(), &a15 }, { a16.get_id(), &a16 }
+    std::unordered_map<int, std::shared_ptr<Package>> package_map = {
+        { a01.get()->get_id(), a01 }, { a02.get()->get_id(), a02 }, { a03.get()->get_id(), a03 }, { a04.get()->get_id(), a04 },
+        { a05.get()->get_id(), a05 }, { a06.get()->get_id(), a06 }, { a07.get()->get_id(), a07 }, { a08.get()->get_id(), a08 },
+        { a09.get()->get_id(), a09 }, { a10.get()->get_id(), a10 }, { a11.get()->get_id(), a11 }, { a12.get()->get_id(), a12 },
+        { a13.get()->get_id(), a13 }, { a14.get()->get_id(), a14 }, { a15.get()->get_id(), a15 }, { a16.get()->get_id(), a16 }
     };
 
     CriticalPathFinder completer{ package_map };
     completer.complete();
 
     // A01
-    EXPECT_EQ(0, a01.get_earliest_start());
-    EXPECT_EQ(3, a01.get_earliest_finish());
-    EXPECT_EQ(0, a01.get_latest_start());
-    EXPECT_EQ(3, a01.get_latest_finish());
-    EXPECT_EQ(0, a01.get_slack());
+    EXPECT_EQ(0, a01.get()->get_earliest_start());
+    EXPECT_EQ(3, a01.get()->get_earliest_finish());
+    EXPECT_EQ(0, a01.get()->get_latest_start());
+    EXPECT_EQ(3, a01.get()->get_latest_finish());
+    EXPECT_EQ(0, a01.get()->get_slack());
 
     // A02
-    EXPECT_EQ(0, a02.get_earliest_start());
-    EXPECT_EQ(4, a02.get_earliest_finish());
-    EXPECT_EQ(2, a02.get_latest_start());
-    EXPECT_EQ(6, a02.get_latest_finish());
-    EXPECT_EQ(2, a02.get_slack());
+    EXPECT_EQ(0, a02.get()->get_earliest_start());
+    EXPECT_EQ(4, a02.get()->get_earliest_finish());
+    EXPECT_EQ(2, a02.get()->get_latest_start());
+    EXPECT_EQ(6, a02.get()->get_latest_finish());
+    EXPECT_EQ(2, a02.get()->get_slack());
 
     // A03
-    EXPECT_EQ(0, a03.get_earliest_start());
-    EXPECT_EQ(5, a03.get_earliest_finish());
-    EXPECT_EQ(3, a03.get_latest_start());
-    EXPECT_EQ(8, a03.get_latest_finish());
-    EXPECT_EQ(3, a03.get_slack());
+    EXPECT_EQ(0, a03.get()->get_earliest_start());
+    EXPECT_EQ(5, a03.get()->get_earliest_finish());
+    EXPECT_EQ(3, a03.get()->get_latest_start());
+    EXPECT_EQ(8, a03.get()->get_latest_finish());
+    EXPECT_EQ(3, a03.get()->get_slack());
 
     // A04
-    EXPECT_EQ(3, a04.get_earliest_start());
-    EXPECT_EQ(7, a04.get_earliest_finish());
-    EXPECT_EQ(4, a04.get_latest_start());
-    EXPECT_EQ(8, a04.get_latest_finish());
-    EXPECT_EQ(1, a04.get_slack());
+    EXPECT_EQ(3, a04.get()->get_earliest_start());
+    EXPECT_EQ(7, a04.get()->get_earliest_finish());
+    EXPECT_EQ(4, a04.get()->get_latest_start());
+    EXPECT_EQ(8, a04.get()->get_latest_finish());
+    EXPECT_EQ(1, a04.get()->get_slack());
 
     // A05
-    EXPECT_EQ(3, a05.get_earliest_start());
-    EXPECT_EQ(12, a05.get_earliest_finish());
-    EXPECT_EQ(3, a05.get_latest_start());
-    EXPECT_EQ(12, a05.get_latest_finish());
-    EXPECT_EQ(0, a05.get_slack());
+    EXPECT_EQ(3, a05.get()->get_earliest_start());
+    EXPECT_EQ(12, a05.get()->get_earliest_finish());
+    EXPECT_EQ(3, a05.get()->get_latest_start());
+    EXPECT_EQ(12, a05.get()->get_latest_finish());
+    EXPECT_EQ(0, a05.get()->get_slack());
 
     // A06
-    EXPECT_EQ(4, a06.get_earliest_start());
-    EXPECT_EQ(8, a06.get_earliest_finish());
-    EXPECT_EQ(6, a06.get_latest_start());
-    EXPECT_EQ(10, a06.get_latest_finish());
-    EXPECT_EQ(2, a06.get_slack());
+    EXPECT_EQ(4, a06.get()->get_earliest_start());
+    EXPECT_EQ(8, a06.get()->get_earliest_finish());
+    EXPECT_EQ(6, a06.get()->get_latest_start());
+    EXPECT_EQ(10, a06.get()->get_latest_finish());
+    EXPECT_EQ(2, a06.get()->get_slack());
 
     // A07
-    EXPECT_EQ(5, a07.get_earliest_start());
-    EXPECT_EQ(7, a07.get_earliest_finish());
-    EXPECT_EQ(8, a07.get_latest_start());
-    EXPECT_EQ(10, a07.get_latest_finish());
-    EXPECT_EQ(3, a07.get_slack());
+    EXPECT_EQ(5, a07.get()->get_earliest_start());
+    EXPECT_EQ(7, a07.get()->get_earliest_finish());
+    EXPECT_EQ(8, a07.get()->get_latest_start());
+    EXPECT_EQ(10, a07.get()->get_latest_finish());
+    EXPECT_EQ(3, a07.get()->get_slack());
 
     // A08
-    EXPECT_EQ(7, a08.get_earliest_start());
-    EXPECT_EQ(11, a08.get_earliest_finish());
-    EXPECT_EQ(8, a08.get_latest_start());
-    EXPECT_EQ(12, a08.get_latest_finish());
-    EXPECT_EQ(1, a08.get_slack());
+    EXPECT_EQ(7, a08.get()->get_earliest_start());
+    EXPECT_EQ(11, a08.get()->get_earliest_finish());
+    EXPECT_EQ(8, a08.get()->get_latest_start());
+    EXPECT_EQ(12, a08.get()->get_latest_finish());
+    EXPECT_EQ(1, a08.get()->get_slack());
 
     // A09
-    EXPECT_EQ(8, a09.get_earliest_start());
-    EXPECT_EQ(10, a09.get_earliest_finish());
-    EXPECT_EQ(12, a09.get_latest_start());
-    EXPECT_EQ(14, a09.get_latest_finish());
-    EXPECT_EQ(4, a09.get_slack());
+    EXPECT_EQ(8, a09.get()->get_earliest_start());
+    EXPECT_EQ(10, a09.get()->get_earliest_finish());
+    EXPECT_EQ(12, a09.get()->get_latest_start());
+    EXPECT_EQ(14, a09.get()->get_latest_finish());
+    EXPECT_EQ(4, a09.get()->get_slack());
 
     // A10
-    EXPECT_EQ(8, a10.get_earliest_start());
-    EXPECT_EQ(11, a10.get_earliest_finish());
-    EXPECT_EQ(10, a10.get_latest_start());
-    EXPECT_EQ(13, a10.get_latest_finish());
-    EXPECT_EQ(2, a10.get_slack());
+    EXPECT_EQ(8, a10.get()->get_earliest_start());
+    EXPECT_EQ(11, a10.get()->get_earliest_finish());
+    EXPECT_EQ(10, a10.get()->get_latest_start());
+    EXPECT_EQ(13, a10.get()->get_latest_finish());
+    EXPECT_EQ(2, a10.get()->get_slack());
 
     // A11
-    EXPECT_EQ(7, a11.get_earliest_start());
-    EXPECT_EQ(10, a11.get_earliest_finish());
-    EXPECT_EQ(10, a11.get_latest_start());
-    EXPECT_EQ(13, a11.get_latest_finish());
-    EXPECT_EQ(3, a11.get_slack());
+    EXPECT_EQ(7, a11.get()->get_earliest_start());
+    EXPECT_EQ(10, a11.get()->get_earliest_finish());
+    EXPECT_EQ(10, a11.get()->get_latest_start());
+    EXPECT_EQ(13, a11.get()->get_latest_finish());
+    EXPECT_EQ(3, a11.get()->get_slack());
 
     // A12
-    EXPECT_EQ(12, a12.get_earliest_start());
-    EXPECT_EQ(14, a12.get_earliest_finish());
-    EXPECT_EQ(12, a12.get_latest_start());
-    EXPECT_EQ(14, a12.get_latest_finish());
-    EXPECT_EQ(0, a12.get_slack());
+    EXPECT_EQ(12, a12.get()->get_earliest_start());
+    EXPECT_EQ(14, a12.get()->get_earliest_finish());
+    EXPECT_EQ(12, a12.get()->get_latest_start());
+    EXPECT_EQ(14, a12.get()->get_latest_finish());
+    EXPECT_EQ(0, a12.get()->get_slack());
 
     // A13
-    EXPECT_EQ(11, a13.get_earliest_start());
-    EXPECT_EQ(15, a13.get_earliest_finish());
-    EXPECT_EQ(13, a13.get_latest_start());
-    EXPECT_EQ(17, a13.get_latest_finish());
-    EXPECT_EQ(2, a13.get_slack());
+    EXPECT_EQ(11, a13.get()->get_earliest_start());
+    EXPECT_EQ(15, a13.get()->get_earliest_finish());
+    EXPECT_EQ(13, a13.get()->get_latest_start());
+    EXPECT_EQ(17, a13.get()->get_latest_finish());
+    EXPECT_EQ(2, a13.get()->get_slack());
 
     // A14
-    EXPECT_EQ(14, a14.get_earliest_start());
-    EXPECT_EQ(21, a14.get_earliest_finish());
-    EXPECT_EQ(14, a14.get_latest_start());
-    EXPECT_EQ(21, a14.get_latest_finish());
-    EXPECT_EQ(0, a14.get_slack());
+    EXPECT_EQ(14, a14.get()->get_earliest_start());
+    EXPECT_EQ(21, a14.get()->get_earliest_finish());
+    EXPECT_EQ(14, a14.get()->get_latest_start());
+    EXPECT_EQ(21, a14.get()->get_latest_finish());
+    EXPECT_EQ(0, a14.get()->get_slack());
 
     // A15
-    EXPECT_EQ(14, a15.get_earliest_start());
-    EXPECT_EQ(16, a15.get_earliest_finish());
-    EXPECT_EQ(15, a15.get_latest_start());
-    EXPECT_EQ(17, a15.get_latest_finish());
-    EXPECT_EQ(1, a15.get_slack());
+    EXPECT_EQ(14, a15.get()->get_earliest_start());
+    EXPECT_EQ(16, a15.get()->get_earliest_finish());
+    EXPECT_EQ(15, a15.get()->get_latest_start());
+    EXPECT_EQ(17, a15.get()->get_latest_finish());
+    EXPECT_EQ(1, a15.get()->get_slack());
 
     // A16
-    EXPECT_EQ(16, a16.get_earliest_start());
-    EXPECT_EQ(20, a16.get_earliest_finish());
-    EXPECT_EQ(17, a16.get_latest_start());
-    EXPECT_EQ(21, a16.get_latest_finish());
-    EXPECT_EQ(1, a16.get_slack());
+    EXPECT_EQ(16, a16.get()->get_earliest_start());
+    EXPECT_EQ(20, a16.get()->get_earliest_finish());
+    EXPECT_EQ(17, a16.get()->get_latest_start());
+    EXPECT_EQ(21, a16.get()->get_latest_finish());
+    EXPECT_EQ(1, a16.get()->get_slack());
 }
 
 TEST(CriticalPath, PackageNetFromLecture) {
-    Package a{ 1, 6, {} };
-    Package b{ 2, 8, {} };
-    Package c{ 3, 5, {} };
-    Package d{ 4, 15, {a.get_id()} };
-    Package e{ 5, 17, {a.get_id()} };
-    Package f{ 6, 13, {b.get_id()} };
-    Package g{ 7, 9, {c.get_id()} };
-    Package h{ 8, 9, {d.get_id()} };
-    Package i{ 9, 6, {e.get_id()} };
-    Package j{ 10, 12, {f.get_id(), g.get_id()} };
+    auto a = std::make_shared<Package>(1, 6, std::vector<int>{});
+    auto b = std::make_shared<Package>(2, 8, std::vector<int>{});
+    auto c = std::make_shared<Package>(3, 5, std::vector<int>{});
+    auto d = std::make_shared<Package>(4, 15, std::vector<int>{a.get()->get_id()});
+    auto e = std::make_shared<Package>(5, 17, std::vector<int>{a.get()->get_id()});
+    auto f = std::make_shared<Package>(6, 13, std::vector<int>{b.get()->get_id()});
+    auto g = std::make_shared<Package>(7, 9, std::vector<int>{c.get()->get_id()});
+    auto h = std::make_shared<Package>(8, 9, std::vector<int>{d.get()->get_id()});
+    auto i = std::make_shared<Package>(9, 6, std::vector<int>{e.get()->get_id()});
+    auto j = std::make_shared<Package>(10, 12, std::vector<int>{f.get()->get_id(), g.get()->get_id()});
 
-    std::unordered_map<int, Package*> package_map_from_lecture = {
-        { a.get_id(), &a }, { b.get_id(), &b }, { c.get_id(), &c }, { d.get_id(), &d },
-        { e.get_id(), &e }, { f.get_id(), &f }, { g.get_id(), &g }, { h.get_id(), &h },
-        { i.get_id(), &i }, { j.get_id(), &j }
+    std::unordered_map<int, std::shared_ptr<Package>> package_map_from_lecture = {
+        { a.get()->get_id(), a }, { b.get()->get_id(), b }, { c.get()->get_id(), c }, { d.get()->get_id(), d },
+        { e.get()->get_id(), e }, { f.get()->get_id(), f }, { g.get()->get_id(), g }, { h.get()->get_id(), h },
+        { i.get()->get_id(), i }, { j.get()->get_id(), j }
     };
 
     CriticalPathFinder completer{ package_map_from_lecture };
     completer.complete();
-    
+
     // A
-    EXPECT_EQ(0, a.get_earliest_start());
-    EXPECT_EQ(6, a.get_earliest_finish());
-    EXPECT_EQ(3, a.get_latest_start());
-    EXPECT_EQ(9, a.get_latest_finish());
-    EXPECT_EQ(3, a.get_slack());
+    EXPECT_EQ(0, a.get()->get_earliest_start());
+    EXPECT_EQ(6, a.get()->get_earliest_finish());
+    EXPECT_EQ(3, a.get()->get_latest_start());
+    EXPECT_EQ(9, a.get()->get_latest_finish());
+    EXPECT_EQ(3, a.get()->get_slack());
 
     // B
-    EXPECT_EQ(0, b.get_earliest_start());
-    EXPECT_EQ(8, b.get_earliest_finish());
-    EXPECT_EQ(0, b.get_latest_start());
-    EXPECT_EQ(8, b.get_latest_finish());
-    EXPECT_EQ(0, b.get_slack());
+    EXPECT_EQ(0, b.get()->get_earliest_start());
+    EXPECT_EQ(8, b.get()->get_earliest_finish());
+    EXPECT_EQ(0, b.get()->get_latest_start());
+    EXPECT_EQ(8, b.get()->get_latest_finish());
+    EXPECT_EQ(0, b.get()->get_slack());
 
     // C
-    EXPECT_EQ(0, c.get_earliest_start());
-    EXPECT_EQ(5, c.get_earliest_finish());
-    EXPECT_EQ(7, c.get_latest_start());
-    EXPECT_EQ(12, c.get_latest_finish());
-    EXPECT_EQ(7, c.get_slack());
+    EXPECT_EQ(0, c.get()->get_earliest_start());
+    EXPECT_EQ(5, c.get()->get_earliest_finish());
+    EXPECT_EQ(7, c.get()->get_latest_start());
+    EXPECT_EQ(12, c.get()->get_latest_finish());
+    EXPECT_EQ(7, c.get()->get_slack());
 
     // D
-    EXPECT_EQ(6, d.get_earliest_start());
-    EXPECT_EQ(21, d.get_earliest_finish());
-    EXPECT_EQ(9, d.get_latest_start());
-    EXPECT_EQ(24, d.get_latest_finish());
-    EXPECT_EQ(3, d.get_slack());
+    EXPECT_EQ(6, d.get()->get_earliest_start());
+    EXPECT_EQ(21, d.get()->get_earliest_finish());
+    EXPECT_EQ(9, d.get()->get_latest_start());
+    EXPECT_EQ(24, d.get()->get_latest_finish());
+    EXPECT_EQ(3, d.get()->get_slack());
 
     // E
-    EXPECT_EQ(6, e.get_earliest_start());
-    EXPECT_EQ(23, e.get_earliest_finish());
-    EXPECT_EQ(10, e.get_latest_start());
-    EXPECT_EQ(27, e.get_latest_finish());
-    EXPECT_EQ(4, e.get_slack());
+    EXPECT_EQ(6, e.get()->get_earliest_start());
+    EXPECT_EQ(23, e.get()->get_earliest_finish());
+    EXPECT_EQ(10, e.get()->get_latest_start());
+    EXPECT_EQ(27, e.get()->get_latest_finish());
+    EXPECT_EQ(4, e.get()->get_slack());
 
     // F
-    EXPECT_EQ(8, f.get_earliest_start());
-    EXPECT_EQ(21, f.get_earliest_finish());
-    EXPECT_EQ(8, f.get_latest_start());
-    EXPECT_EQ(21, f.get_latest_finish());
-    EXPECT_EQ(0, f.get_slack());
+    EXPECT_EQ(8, f.get()->get_earliest_start());
+    EXPECT_EQ(21, f.get()->get_earliest_finish());
+    EXPECT_EQ(8, f.get()->get_latest_start());
+    EXPECT_EQ(21, f.get()->get_latest_finish());
+    EXPECT_EQ(0, f.get()->get_slack());
 
     // G
-    EXPECT_EQ(5, g.get_earliest_start());
-    EXPECT_EQ(14, g.get_earliest_finish());
-    EXPECT_EQ(12, g.get_latest_start());
-    EXPECT_EQ(21, g.get_latest_finish());
-    EXPECT_EQ(7, g.get_slack());
+    EXPECT_EQ(5, g.get()->get_earliest_start());
+    EXPECT_EQ(14, g.get()->get_earliest_finish());
+    EXPECT_EQ(12, g.get()->get_latest_start());
+    EXPECT_EQ(21, g.get()->get_latest_finish());
+    EXPECT_EQ(7, g.get()->get_slack());
 
     // H
-    EXPECT_EQ(21, h.get_earliest_start());
-    EXPECT_EQ(30, h.get_earliest_finish());
-    EXPECT_EQ(24, h.get_latest_start());
-    EXPECT_EQ(33, h.get_latest_finish());
-    EXPECT_EQ(3, h.get_slack());
+    EXPECT_EQ(21, h.get()->get_earliest_start());
+    EXPECT_EQ(30, h.get()->get_earliest_finish());
+    EXPECT_EQ(24, h.get()->get_latest_start());
+    EXPECT_EQ(33, h.get()->get_latest_finish());
+    EXPECT_EQ(3, h.get()->get_slack());
 
     // I
-    EXPECT_EQ(23, i.get_earliest_start());
-    EXPECT_EQ(29, i.get_earliest_finish());
-    EXPECT_EQ(27, i.get_latest_start());
-    EXPECT_EQ(33, i.get_latest_finish());
-    EXPECT_EQ(4, i.get_slack());
+    EXPECT_EQ(23, i.get()->get_earliest_start());
+    EXPECT_EQ(29, i.get()->get_earliest_finish());
+    EXPECT_EQ(27, i.get()->get_latest_start());
+    EXPECT_EQ(33, i.get()->get_latest_finish());
+    EXPECT_EQ(4, i.get()->get_slack());
 
     // J
-    EXPECT_EQ(21, j.get_earliest_start());
-    EXPECT_EQ(33, j.get_earliest_finish());
-    EXPECT_EQ(21, j.get_latest_start());
-    EXPECT_EQ(33, j.get_latest_finish());
-    EXPECT_EQ(0, j.get_slack());
+    EXPECT_EQ(21, j.get()->get_earliest_start());
+    EXPECT_EQ(33, j.get()->get_earliest_finish());
+    EXPECT_EQ(21, j.get()->get_latest_start());
+    EXPECT_EQ(33, j.get()->get_latest_finish());
+    EXPECT_EQ(0, j.get()->get_slack());
 }
